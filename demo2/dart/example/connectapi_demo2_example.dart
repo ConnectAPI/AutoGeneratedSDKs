1ebc87200bd5a949c2b395909a0ada571148ba97
hvuhsg <34660046+hvuhsg@users.noreply.github.com>
2022-01-10 21:33:08 +0200

    Delete demo2 directory

--- demo2/dart/example/connectapi_demo2_example.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mypackage/mypackage.dart';
-
-void main() {
-  // TODO: ADD example
-}
--- demo2/dart/lib/connectapi_demo2.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library connectapi_demo2;
-
-export 'src/connectapi_demo2_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
