f1ef1aa96ad34a8bc6577dab18305ca2a2352717
hvuhsg6@gmail.com <hvuhsg6@gmail.com>
2022-01-19 14:26:27 +0000

    Generated SDKs for service

---
+++ demo/python/connectapi_demo/schemas.py
@@ -0,0 +1,119 @@
+"""
+Auto generated file, DO NOT EDIT!
+"""
+
+from typing import List
+
+__all__ = ['BodyFileOptional', 'BodyFile', 'BodyFormOptional', 'BodyForm', 'HTTPValidationError', 'Nested', 'TestIn', 'ValidationError', ]
+
+
+class BodyFileOptional:
+    def __init__(self, a: str = None, ):
+        self.a = a
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class BodyFile:
+    def __init__(self, a: str, ):
+        self.a = a
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class BodyFormOptional:
+    def __init__(self, a: str = None, ):
+        self.a = a
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class BodyForm:
+    def __init__(self, a: str, ):
+        self.a = a
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class HTTPValidationError:
+    def __init__(self, detail: List = None, ):
+        self.detail = detail
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class Nested:
+    def __init__(self, a: int = None, ):
+        self.a = a
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class TestIn:
+    def __init__(self, a: int, b: str, c: bool, d: None, e: object, f: List, g, a_o: int = None, b_o: str = None, c_o: bool = None, d_o: None = None, e_o: object = None, f_o: List = None, g_o=None, ):
+        self.a = a
+        self.a_o = a_o
+        self.b = b
+        self.b_o = b_o
+        self.c = c
+        self.c_o = c_o
+        self.d = d
+        self.d_o = d_o
+        self.e = e
+        self.e_o = e_o
+        self.f = f
+        self.f_o = f_o
+        self.g = g
+        self.g_o = g_o
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
+
+class ValidationError:
+    def __init__(self, loc: List, msg: str, type: str, ):
+        self.loc = loc
+        self.msg = msg
+        self.type = type
+        
+    def to_dict(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_dict(cls, dict_):
+        return cls(**dict_)
+
