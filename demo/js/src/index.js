9f8a6882c34b55daa53b87635ef0226cd83beadf
hvuhsg6@gmail.com <hvuhsg6@gmail.com>
2022-02-12 20:11:10 +0000

    Generated SDKs for service

---
+++ demo/js/src/index.js
@@ -0,0 +1,5 @@
+// Auto generated file, DO NOT EDIT!
+
+
+export { Demo } from './connectapi_demo/demo';
+export * from './connectapi_demo/schemas';
---
+++ demo/python/connectapi_demo/__init__.py
@@ -0,0 +1,16 @@
+"""
+Auto generated file, DO NOT EDIT!
+"""
+
+from .demo import Demo
+
+from .schemas import (
+    BodyFileOptional,
+    BodyFile,
+    BodyFormOptional,
+    BodyForm,
+    HTTPValidationError,
+    Nested,
+    TestIn,
+    ValidationError,
+    )
